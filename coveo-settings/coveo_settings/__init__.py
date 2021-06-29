3b99402bf945ebe713a363bc00f13841b6fd4382
Jonathan Piché <jpiche@coveo.com>
2021-06-29 10:51:43 -0400

    settings redirection

--- coveo-settings/coveo_settings/__init__.py
+++ coveo-settings/coveo_settings/__init__.py
@@ -4,5 +4,5 @@ from coveo_settings.dict_setting import DictSetting
 from coveo_settings.float_setting import FloatSetting
 from coveo_settings.int_setting import IntSetting
 from coveo_settings.path_setting import PathSetting
-from coveo_settings.setting_abc import Setting
+from coveo_settings.setting_abc import Setting, settings_adapter, ConfigValue
 from coveo_settings.string_setting import StringSetting
