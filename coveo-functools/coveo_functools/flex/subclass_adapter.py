bf6b8542ac0795c672b766192fea60d65d19394b
Jonathan Piché <jpiche@coveo.com>
2021-09-03 07:25:58 -0400

    "moi j'aime pas les cast"
    - un shtroumpf

--- coveo-functools/coveo_functools/flex/subclass_adapter.py
+++ coveo-functools/coveo_functools/flex/subclass_adapter.py
@@ -1,23 +1,25 @@
 from typing import Dict, Type, Callable, Any, Optional, TypeVar
 
+from coveo_functools.flex.types import TypeHint
+
 
 T = TypeVar("T")
 
 _subclass_adapters: Dict[Type, Callable[[Any], Type]] = {}
 
 
-def register_subclass_adapter(base_class: Type, adapter: Callable[[Any], Type]) -> None:
+def register_subclass_adapter(hint: TypeHint, adapter: Callable[[Any], Type]) -> None:
     """
     Registers a custom callback for a type. This is necessary when an annotation is abstract.
 
     The callback will receive the raw payload value. It should inspect this payload and return the appropriate type
     for deserialization.
     """
-    if base_class in _subclass_adapters:
+    if hint in _subclass_adapters:
         raise RuntimeError("An adapter for this class was already registered.")
 
-    _subclass_adapters[base_class] = adapter
+    _subclass_adapters[hint] = adapter
 
 
-def get_subclass_adapter(base_class: Type[T]) -> Optional[Callable[[Any], Type[T]]]:
-    return _subclass_adapters.get(base_class)
+def get_subclass_adapter(hint: TypeHint) -> Optional[Callable[[Any], Type[T]]]:
+    return _subclass_adapters.get(hint)
